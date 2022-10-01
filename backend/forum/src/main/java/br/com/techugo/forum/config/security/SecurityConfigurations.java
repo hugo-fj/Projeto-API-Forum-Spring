37d18717c3e6d2b9c691c3203859c501a722a4a7
Hugo <phugofj@gmail.com>
2022-09-30 21:34:19 -0300

    Ativado o monitoramento via Spring Boot Actuator e interface Spring Boot Admin

--- backend/forum/src/main/java/br/com/techugo/forum/config/security/SecurityConfigurations.java
+++ backend/forum/src/main/java/br/com/techugo/forum/config/security/SecurityConfigurations.java
@@ -50,6 +50,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/topicos").permitAll()
                 .antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+                .antMatchers(HttpMethod.GET,"/actuator/**").permitAll()
                 .antMatchers(HttpMethod.POST,"/auth").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
